ad9e657b701ff7191c216c59c31aed1c59b51bff
CodingManoj <mvp2612@hotmail.com>
2023-04-18 23:10:48 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source        = "./vendor/modules/vpc"
-  VPCCIDR       = "${var.VPCCIDR}"
+  VPCCIDR       = "var.VPCCIDR"
 }
 
 # This always goes with the main branch, so how can I tell this module pick the code from the intended branch
