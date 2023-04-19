a4ce59d213b0359bf386c96e12ceb3be7e9ff50f
CodingManoj <mvp2612@hotmail.com>
2023-04-18 22:45:00 -0400

    Git Auto Push

---
+++ env-prod/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "b53-tfstate-bucket"
+key    = "vpc/prod/terraform.tfstate"
+region = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/b53-clouddevops/tf-module-vpc.git?ref=main"
+  source = "vendor/modules/vpc"
 }
 
 # This always goes with the main branch, so how can I tell this module pick the code from the intended branch
