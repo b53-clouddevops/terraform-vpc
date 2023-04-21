27b10db539d2a1e0bcc453d2e4573aadcd74866f
CodingManoj <mvp2612@hotmail.com>
2023-04-20 22:52:54 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ module "vpc" {
   source                 = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   VPC_CIDR               = var.VPC_CIDR
   ENV                    = var.ENV
+  PUBLIC_SUBNET_CIDR     = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR    = var.PRIVATE_SUBNET_CIDR 
+  AZ                     = var.AZ
+
 }
